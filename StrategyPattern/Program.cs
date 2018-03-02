b292f72163924cd2d85fddb7ebd124e37f83928c
Jason Vipond <jwvipond@gmail.com>
2018-03-01 23:32:10 -0600

    Added some classes and and an interface to implement simple "play behavior" functionality.

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -10,6 +10,63 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
+            TerrierPuppy myPuppy = new TerrierPuppy();
+
+            myPuppy.PerformPlay();
+
+            TugOfWarPlay myPlayBehavior = new TugOfWarPlay();
+            myPuppy.SetPlayBehavior(myPlayBehavior);
+            myPuppy.PerformPlay();
+            Console.ReadKey();
         }
     }
+    public interface IPlayBehavior
+    {
+        void Play();
+    }
+
+    public class FetchPlay : IPlayBehavior
+    {
+        public void Play()
+        {
+            Console.WriteLine("Puppy plays fetch!");
+        }
+    }
+
+    public class TugOfWarPlay : IPlayBehavior
+    {
+        public void Play()
+        {
+            Console.WriteLine("Puppy plays tug of war!");
+        }
+    }
+
+    public abstract class Puppy
+    {
+        protected IPlayBehavior playBehavior;
+
+        public Puppy()
+        {
+
+        }
+
+        public void SetPlayBehavior(IPlayBehavior pb)
+        {
+            playBehavior = pb;
+        }
+
+        public void PerformPlay()
+        {
+            playBehavior.Play();
+        }
+    }
+
+    public class TerrierPuppy : Puppy
+    {      
+        public TerrierPuppy()
+        {
+            playBehavior = new FetchPlay();
+        }
+    }
+
 }
